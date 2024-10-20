9c5a1159faefe4e1ec2eb2c95314446c816bdc69
tillwenke <till.wenke@gmx.net>
2024-10-20 22:03:35 +0200

    feat: dummy login interface

--- includes/Special/SpecialBoilerPlate.php
+++ includes/Special/SpecialBoilerPlate.php
@@ -17,7 +17,29 @@ class SpecialBoilerPlate extends SpecialPage {
 
 		# Do stuff
 		# ...
-		$wikitext = 'Hello world!';
+		$wikitext = 'Hello '. $par;
 		$output->addWikiTextAsInterface( $wikitext );
+		// Add a basic form with a button
+		$html = '<form method="post" action="' . htmlspecialchars($this->getPageTitle()->getLocalURL()) . '">';
+		$html .= '<input type="hidden" name="action" value="submit_form">';
+		$html .= '<input type="submit" value="Nostr Extension Login" class="mw-ui-button mw-ui-progressive">';
+		$html .= '</form>';
+
+		// Add the HTML to the output
+		$output->addHTML($html);
+
+		// Optionally, check if the form is submitted
+		$this->handleFormSubmission();
+	}
+
+	// Handle form submission
+	private function handleFormSubmission() {
+		// Check if the form has been submitted
+		$request = $this->getRequest();
+		if ($request->getVal('action') === 'submit_form') {
+			// Perform some action upon form submission
+			$output = $this->getOutput();
+			$output->addWikiTextAsContent("'''Form Submitted!'''");
+		}
 	}
 }
